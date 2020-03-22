5e1e6ebcaf136e383782c5c09844fda975db2a84
Nazirok <nazir_06@mail.ru>
2020-03-22 17:54:06 +0300

    feat: add fields to tree basic structs

--- btree.go
+++ btree.go
@@ -1,7 +1,17 @@
 package btree
 
+const t = 5
+
+// Btree B-tree struct
 type Btree struct {
+	root *bNode
 }
 
-type BNode struct {
+type bNode struct {
+	kcount int64
+	scount int64
+	keys   [2*t - 1]int64
+	childs [2 * t]*bNode
+	parent *bNode
+	leaf   bool
 }
